33548622f8786fd71492796e310d51a324f870f3
MIKE LE MINH NGUYEN <minhnl@yahoo.com>
2022-01-19 13:38:28 -0500

    initial implementation for setupInput and handleUserInput

--- play.js
+++ play.js
@@ -10,3 +10,21 @@ conn.on('data', (data) => {
 conn.on("connect", () => {
   conn.write("Name: Le");
 });
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const stdin = setupInput();
+stdin.on("data", handleUserInput);
